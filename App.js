6596191d6eb71a68b97b684af70c59774ba72a29
PedroAsvdo <76012876+objpedro@users.noreply.github.com>
2023-05-21 15:27:36 -0300

    navegacao de telas

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { SignUpScreen } from './src/pages/SignUpScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { Routes } from './src/routes/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <SignUpScreen />
+    <>
+      <NavigationContainer>
+        <Routes />
+      </NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/pages/SignInScreen/index.js
+++ src/pages/SignInScreen/index.js
@@ -4,69 +4,74 @@ import {
     Text,
     Image,
     ImageBackground,
-    TouchableOpacity
+    TouchableOpacity,
+    ScrollView
 } from 'react-native';
 import { TextField } from "../../components/TextField";
 import { Mail } from 'react-native-feather';
 import { styles } from "./styles";
 import colors from "../../utils/colors";
+import { useNavigation } from "@react-navigation/core";
 
 export function SignInScreen() {
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
+    const navigation = useNavigation();
 
     return (
         <View style={styles.container}>
-            <ImageBackground
-                source={require('../../../assets/banner_inicio.png')}
-                imageStyle={styles.imageStyle}
-                style={styles.imageBackground}>
-                <Image style={styles.imageLogo}
-                    source={require('../../../assets/ecopoint_logo.png')} />
-                <View style={styles.header}>
-                    <Text style={styles.textWelcome}>Seja bem vindo</Text>
-                    <Text style={styles.textLogin}>Faça login na sua conta</Text>
-                </View>
-            </ImageBackground>
+            <ScrollView contentContainerStyle={styles.contentContainer}>
+                <ImageBackground
+                    source={require('../../../assets/banner_inicio.png')}
+                    imageStyle={styles.imageStyle}
+                    style={styles.imageBackground}>
+                    <Image style={styles.imageLogo}
+                        source={require('../../../assets/ecopoint_logo.png')} />
+                    <View style={styles.header}>
+                        <Text style={styles.textWelcome}>Seja bem vindo</Text>
+                        <Text style={styles.textLogin}>Faça login na sua conta</Text>
+                    </View>
+                </ImageBackground>
 
-            <View style={styles.inputsContainer}>
-                <TextField
-                    icon={<Mail
-                        style={styles.iconField}
-                        width={15}
-                        height={15}
-                        color={colors.primario} />}
-                    onChange={setEmail}
-                    value={email}
-                    placeholder={'Email'}
-                    isPassword={false}
-                />
+                <View style={styles.inputsContainer}>
+                    <TextField
+                        icon={<Mail
+                            style={styles.iconField}
+                            width={15}
+                            height={15}
+                            color={colors.primario} />}
+                        onChange={setEmail}
+                        value={email}
+                        placeholder={'Email'}
+                        isPassword={false}
+                    />
 
-                <TextField
-                    onChange={setSenha}
-                    value={senha}
-                    placeholder={'Senha'}
-                    isPassword={true}
-                />
+                    <TextField
+                        onChange={setSenha}
+                        value={senha}
+                        placeholder={'Senha'}
+                        isPassword={true}
+                    />
 
-                <TouchableOpacity style={styles.btnEsqueceuSenha}
-                    onPress={() => { console.log('Esqueci a senha') }}>
-                    <Text style={styles.textEsqueceuSenha}>Esqueceu sua senha?</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity style={styles.btnLogin}
-                    onPress={() => { console.log('Login') }} >
-                    <Text style={styles.txtBtnLogin}>Login</Text>
-                </TouchableOpacity>
-
-                <View style={styles.cadastreseContainer}>
-                    <Text>É seu primeiro acesso? </Text>
-                    <TouchableOpacity style={styles.btnCadastrese}
-                        onPress={() => { console.log('Cadastre-se') }}>
-                        <Text style={styles.textEsqueceuSenha}>Cadastre-se aqui.</Text>
+                    <TouchableOpacity style={styles.btnEsqueceuSenha}
+                        onPress={() => { console.log('Esqueci a senha') }}>
+                        <Text style={styles.textEsqueceuSenha}>Esqueceu sua senha?</Text>
                     </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.btnLogin}
+                        onPress={() => { console.log('Login') }} >
+                        <Text style={styles.txtBtnLogin}>Login</Text>
+                    </TouchableOpacity>
+
+                    <View style={styles.cadastreseContainer}>
+                        <Text>É seu primeiro acesso? </Text>
+                        <TouchableOpacity style={styles.btnCadastrese}
+                            onPress={() => { navigation.navigate('SignUp'); }}>
+                            <Text style={styles.textEsqueceuSenha}>Cadastre-se aqui.</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
-            </View>
+            </ScrollView>
         </View>
     )
 }
