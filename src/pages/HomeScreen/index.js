922a7d22e012477c2b777745370f15f39583e831
Pedro Asevedo <pedrovictortec.ti@gmail.com>
2023-06-04 18:42:52 -0300

    estilo botao

--- src/pages/HomeScreen/index.js
+++ src/pages/HomeScreen/index.js
@@ -1,11 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import MapView from 'react-native-maps';
-import { Marker } from 'react-native-maps';
 import {
     View,
-    Text,
-    Platform,
-    PermissionsAndroid,
 } from 'react-native';
 import * as Location from 'expo-location';
 import { HeaderEcopointer } from '../../components/HeaderEcopointer'
@@ -21,13 +17,11 @@ export function HomeScreen() {
 
     useEffect(() => {
         (async () => {
-
             let { status } = await Location.requestForegroundPermissionsAsync();
             if (status !== 'granted') {
                 setErrorMsg('Permission to access location was denied');
                 return;
             }
-
             let location = await Location.getCurrentPositionAsync({});
             setLocation(location);
         })();
@@ -41,52 +35,67 @@ export function HomeScreen() {
     }
 
     return (
-        <View style={styles.container}>
+        <>
             <HeaderEcopointer />
-            <MapView
-                style={styles.map}
-                region={location}
-                zoomEnabled={true}
-                showsUserLocation={true}
-                loadingEnabled={true}
-            />
-
-            <View style={{
-                position: 'absolute',
-                marginTop: RFValue(96),
-                marginLeft: RFValue(10),
-                width: '80%',
-                flexDirection: 'row',
-            }}>
-                <GooglePlacesAutocomplete
-                    placeholder='Encontre Ecopoints'
-                    onPress={(data, details = null) => {
-                        // 'details' is provided when fetchDetails = true
-                        // console.log(data, details);
-                        console.log('LAT selected: ', details.geometry.location.lat);
-                        console.log('LOG selected: ', details.geometry.location.lng);
-                        setLocation({
-                            latitude: details.geometry.location.lat,
-                            longitude: details.geometry.location.lng,
-                            latitudeDelta: 0.0922,
-                            longitudeDelta: 0.0421,
-                        });
-                    }}
-                    query={{
-                        key: 'AIzaSyD_dcMTz21vH7x0ylilwPxPzXZmVBVjsIA',
-                        language: 'pt-br',
-                    }}
-                    enablePoweredByContainer={false}
-                    fetchDetails={true}
-                    styles={{
-                        listView: { height: 100 }
-                    }}
+            <View style={styles.container}>
+                <MapView
+                    style={styles.map}
+                    region={location}
+                    zoomEnabled={true}
+                    showsUserLocation={true}
+                    loadingEnabled={true}
                 />
-                <Search
-                    width={15}
-                    height={15}
-                    color={colors.primario} />
+                <View style={{
+                    position: 'absolute',
+                    marginTop: RFValue(10),
+                    marginLeft: RFValue(10),
+                    width: '80%',
+                    flexDirection: 'row',
+                    justifyContent: 'flex-end',
+                }}>
+                    <GooglePlacesAutocomplete
+                        placeholder='Encontre Ecopoints'
+                        onPress={(data, details = null) => {
+                            // 'details' is provided when fetchDetails = true
+                            // console.log(data, details);
+                            console.log('LAT selected: ', details.geometry.location.lat);
+                            console.log('LOG selected: ', details.geometry.location.lng);
+                            setLocation({
+                                latitude: details.geometry.location.lat,
+                                longitude: details.geometry.location.lng,
+                                latitudeDelta: 0.0922,
+                                longitudeDelta: 0.0421,
+                            });
+                        }}
+                        query={{
+                            key: 'AIzaSyD_dcMTz21vH7x0ylilwPxPzXZmVBVjsIA',
+                            language: 'pt-br',
+                        }}
+                        enablePoweredByContainer={false}
+                        fetchDetails={true}
+                        styles={{
+                            textInput: {
+                                borderWidth: 3,
+                                borderColor: colors.primario,
+                                borderRadius: 10,
+                            },
+                            listView: { height: 100 }
+                        }}
+                    />
+                    <View style={{
+                        position: 'absolute',
+                    }}>
+                        <Search
+                            style={{
+                                marginRight: RFValue(20),
+                                marginTop: RFValue(12),
+                            }}
+                            width={20}
+                            height={20}
+                            color={colors.primario} />
+                    </View>
+                </View>
             </View>
-        </View>
+        </>
     );
 }
